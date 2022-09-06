75cf2cdf453e80c1c99729d043c9da90506e9c8a
Igor <mr.igor187@gmail.com>
2022-09-06 09:44:33 +0200

    create eventListener which allows users to add new books to library array

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 let library = [];
 
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this. read = read;
+}
+
 const addBookButton = document.querySelector('.add');
 
 addBookButton.addEventListener('click', () => {
-    
+    let bookTitle = prompt("title");
+    let bookAuthor = prompt("author");
+    let bookPages = prompt("pages");
+    let bookRead = prompt('read');
+
+    let newBook = new Book(bookTitle, bookAuthor, bookPages, bookRead);
+    library.push(newBook);
+    console.log(newBook);
+    console.log(library);
 });
 
 function addBooks() {
